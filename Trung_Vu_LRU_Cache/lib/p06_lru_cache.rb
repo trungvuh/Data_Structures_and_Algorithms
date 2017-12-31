c4bb16a25ec21693685267549762c64dae502124
TV <trungvuh@gmail.com>
2017-12-31 11:32:48 -0800

    Phase 6 LinkedList

--- Trung_Vu_LRU_Cache/lib/p06_lru_cache.rb
+++ Trung_Vu_LRU_Cache/lib/p06_lru_cache.rb
@@ -2,7 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
-  attr_reader :count
+  # attr_reader :count
+
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -15,6 +16,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      node = @map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -25,12 +33,24 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+
+    eject! if count == @max
+    @map[key] = @store.append(key, val)
+    val
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node.remove
+    @map[node.key] = @store.append(node.key, node.val)
+    # @store.remove(node.key)
   end
 
   def eject!
+    first = @store.first #first will be a node
+    first.remove
+    @map.delete(first.key)
+    # @store.remove(first.key)
   end
 end
--- Trung_Vu_LRU_Cache/spec/p07_practical_spec.rb
+++ Trung_Vu_LRU_Cache/spec/p07_practical_spec.rb
@@ -3,7 +3,7 @@ require 'p07_practical'
 
 describe '#can_string_be_palindrome?' do
   it "detects whether a string can be configured to a palindrome" do
-    result_1 = can_string_be_palindrome?("edified")
+    result_1 = can_string_be_palindrome?("edifide")
     result_2 = can_string_be_palindrome?("apple")
     result_3 = can_string_be_palindrome?("racecar")
     result_4 = can_string_be_palindrome?("noon")
