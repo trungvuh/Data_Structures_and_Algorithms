8a2c930c987194effafa85cf4fab4e443bbd6bfa
TV <trungvuh@gmail.com>
2017-12-30 20:34:29 -0800

    Phase 4 Linked List

--- Trung_Vu_LRU_Cache/lib/p04_linked_list.rb
+++ Trung_Vu_LRU_Cache/lib/p04_linked_list.rb
@@ -15,11 +15,23 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    self.next = nil
+    self.prev = nil
+    self
   end
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +40,73 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    # self.each do |node|
+    #   if node.key == key
+    #     return true
+    #   end
+    # end
+    # false
+    self.any? { |node| node.key == key }
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+
+    @tail.prev.next = node
+    node.prev = @tail.prev
+    node.next = @tail
+    @tail.prev = node
+
+    node
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
   end
 
   def each
+    current_node = @head.next
+    until current_node == @tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end
