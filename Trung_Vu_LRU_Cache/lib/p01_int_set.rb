feeea1b4f6aee7c62ec8daf62f3cdd945d2d2322
TV <trungvuh@gmail.com>
2017-12-30 18:24:03 -0800

    Phase 1 of LRU

--- Trung_Vu_LRU_Cache/lib/p01_int_set.rb
+++ Trung_Vu_LRU_Cache/lib/p01_int_set.rb
@@ -1,22 +1,30 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num > -1 && num <= @max
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +35,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num].push(num) unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -47,7 +59,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -55,18 +67,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+    unless include?(num)
+      self[num].push(num)
+      @count += 1
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +97,15 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self.insert(el)
+      end
+    end
+
   end
 end
