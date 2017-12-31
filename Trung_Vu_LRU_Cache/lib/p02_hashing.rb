ca877e3aab399ee062674e30fb2b795988ef29a9
TV <trungvuh@gmail.com>
2017-12-30 18:38:20 -0800

    Phase 2 Hashing

--- Trung_Vu_LRU_Cache/lib/p02_hashing.rb
+++ Trung_Vu_LRU_Cache/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hash_sum = 0
+    self.each_index do |i|
+      hash_sum += (self[i].hash + i).hash
+    end
+    hash_sum
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by(&:hash).hash
   end
 end
