72cef3b7a9ae17e0fed10e59b3bb785293047fd0
TV <trungvuh@gmail.com>
2017-12-30 19:34:11 -0800

    Phase 3 Hashmap

--- Trung_Vu_LRU_Cache/lib/p03_hash_set.rb
+++ Trung_Vu_LRU_Cache/lib/p03_hash_set.rb
@@ -9,18 +9,29 @@ class HashSet
   end
 
   def insert(key)
-  end
-
-  def include?(key)
+    resize! if @count == num_buckets
+    unless include?(key)
+      self[key].push(key.hash)
+      @count += 1
+    end
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key.hash)
+      @count -= 1
+    end
+  end
+
+  def include?(key)
+    self[key].include?(key.hash)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +39,14 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.each do |bucket|
+      bucket.each do |el|
+        @store.insert(el)
+      end
+    end
   end
 end
