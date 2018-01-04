9eae6e8298b98f523a00ecab87c1949ade65e858
TV <trungvuh@gmail.com>
2018-01-04 09:16:29 -0800

    Finish Heaps Project

--- Trung_Vu_Heaps_Sort/lib/k_largest_elements.rb
+++ Trung_Vu_Heaps_Sort/lib/k_largest_elements.rb
@@ -2,7 +2,7 @@ require_relative 'heap'
 
 def k_largest_elements(array, k)
   #reverse the heap sort so that we have max heap instead of min heap
-  prc = Proc.new { |a, b| -1 * (a <=> b) }
+  prc = Proc.new { |a, b| b <=> a }
   # this is the heapify step
   array.length.times do |i|
     BinaryMinHeap.heapify_up(array, i, i + 1, &prc)
