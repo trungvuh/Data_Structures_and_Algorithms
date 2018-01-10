c589bfec9cae47d33bda0fc687d9048a8e972dde
TV <trungvuh@gmail.com>
2018-01-09 23:52:24 -0800

    Frog Hop Bottom Up and Top Down

--- Trung_Vu_Dynamic-Programming/lib/dynamic_programming.rb
+++ Trung_Vu_Dynamic-Programming/lib/dynamic_programming.rb
@@ -2,6 +2,11 @@ class DynamicProgramming
 
   def initialize
     @blair_cache = { 1 => 1, 2 => 2 }
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[1, 1], [2]],
+      3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
+    }
   end
 
   def blair_nums(n)
@@ -19,7 +24,10 @@ class DynamicProgramming
   end
 
   def frog_hops_bottom_up(n)
-
+    # call the helper function
+    frog_cache = frog_cache_builder(n)
+    # then return the nth entry
+    frog_cache[n]
   end
 
   def frog_cache_builder(n)
@@ -32,17 +40,46 @@ class DynamicProgramming
       2 => [[1, 1], [2]],
       3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
     }
-    return frog_cache[n] if n <= 3
+    return frog_cache if n <= 3
 
-    
+    # super janky way
+    (4..n).each do |i|
+      frog_cache[i] = []
+      (1..3).each do |j|
+        frog_cache[i - j].each do |el1|
+          frog_cache[j].each do |el2|
+            frog_cache[i] << el1 + el2
+          end
+        end
+      end
+      frog_cache[i].uniq!
+    end
+    frog_cache
   end
 
   def frog_hops_top_down(n)
-
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
+    return @frog_cache[n] if n <= 3
 
+    # iterate through each of the pair (n-1)&1, (n-2)&2, (n-3)&3.
+    # Find every possible combinations between them.
+    # Not very efficient. There must be other ways.
+    # even though using a hash => O(1) lookup, still feel this one is pretty bad
+    # this is probably not dynamic way.
+
+    result = {}
+    (1..3).each do |i|
+      frog_hops_top_down_helper(n - i).each do |el1|
+        frog_hops_top_down_helper(i).each do |el2|
+          result[el1 + el2] = true
+        end
+      end
+    end
+    @frog_cache[n] = result.keys
+    result.keys
   end
 
   def super_frog_hops(n, k)
