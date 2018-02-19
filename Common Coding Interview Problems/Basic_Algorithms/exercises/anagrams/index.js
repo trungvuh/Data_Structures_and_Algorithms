684f5c77d8f0ac9e2d1f326602670c7da2326e9a
TV <trungvuh@gmail.com>
2018-02-18 20:54:48 -0800

    Adding regexp for anagrams problem

--- Common Coding Interview Problems/Basic_Algorithms/exercises/anagrams/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/anagrams/index.js
@@ -8,6 +8,57 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  // strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+  
+  // if (strA.length != strB.length) return false;
+
+  // let counterHashA = {};
+  // let counterHashB = {};
+
+  // for (const element of strA) {
+  //   if (counterHashA[element]) {
+  //     counterHashA[element] += 1;
+  //   }
+  //   else {
+  //     counterHashA[element] = 1;
+  //   }
+  // }
+
+  // for (const element of strB) {
+  //   if (counterHashB[element]) {
+  //     counterHashB[element] += 1;
+  //   }
+  //   else {
+  //     counterHashB[element] = 1;
+  //   }
+  // }
+
+  counterA = counterHash(stringA);
+  counterB = counterHash(stringB);
+  
+  if (Object.keys(counterA).length != Object.keys(counterB).length) return false;
+
+  for (const char in counterA) {
+    if (counterA[char] !== counterB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function counterHash(string) {
+  const counterHash = {};
+
+  str = string.replace(/[^\w]/g, "").toLowerCase();
+  
+  for (const element of str) {
+    counterHash[element] = counterHash[element] + 1 || 1
+  }
+
+  return counterHash;
+}
 
 module.exports = anagrams;
