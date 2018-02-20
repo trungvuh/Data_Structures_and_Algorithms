e32c44e95ac35b6438a2d6b680694fd728d529d9
TV <trungvuh@gmail.com>
2018-02-19 20:40:15 -0800

    Adding spiral problems

--- Common Coding Interview Problems/Basic_Algorithms/exercises/matrix/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  var result = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  let counter = 1;
+  let start_row = 0;
+  let start_col = 0;
+  let end_row = n - 1;
+  let end_col = n - 1;
+
+  //we can also do while (counter <= n**2), notice the counter run from 1=>n square
+  while (start_col <= end_col && start_row <= end_row) {
+    // this is the top side of the matrix
+    for (let i = start_col; i <= end_col; i++) {
+      result[start_row][i] = counter;
+      counter++;
+    }
+    start_row++; //we are done with the top side
+
+    //this is for the right side
+    for (let i = start_row; i <= end_row; i++) {
+      result[i][end_col] = counter;
+      counter++;
+    }
+    end_col--; //we are done with the right side
+
+    //this is for the bottom side
+    for (let i = end_col; i >= start_col; i--) {
+      result[end_row][i] = counter;
+      counter++;
+    }
+    end_row--; //we are done with the bottom side
+
+    for (let i = end_row; i >= start_row; i--) {
+      result[i][start_col] = counter;
+      counter++;
+    }
+    start_col++;
+  }
+
+  return result;
+}
 
 module.exports = matrix;
--- Common Coding Interview Problems/Basic_Algorithms/exercises/vowels/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = 'aeiou'.split('');
+  let count = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count += 1;
+    }
+  }
+
+  return count;
+
+  // RegExp method:
+  // const matches = str.match(/[aeiou]/gi); //g meaning we don't stop at first found, keep going. i is for case insensitive
+  // return matches ? matches.length : 0; //match method either return an array of found items, or return null.
+}
 
 module.exports = vowels;
