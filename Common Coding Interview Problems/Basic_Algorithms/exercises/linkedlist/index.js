e80f4f22d37ca68a0b3f5515f278e7f0d3cbdc4f
TV <trungvuh@gmail.com>
2018-02-25 02:39:14 -0800

    Adding generator for Linked List

--- Common Coding Interview Problems/Basic_Algorithms/exercises/linkedlist/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/linkedlist/index.js
@@ -36,8 +36,11 @@ class LinkedList {
   
   getLast() {
     var node = this.head;
-    while(node.next) {
-      node = node.next;
+
+    if (node) {
+      while(node.next) {
+        node = node.next;
+      }
     }
 
     return node;
@@ -45,6 +48,103 @@ class LinkedList {
 
   clear() {
     this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.removeFirst();
+      return;
+    }
+
+    var previous = this.head;
+    var node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  } 
+  
+  insertLast(data) {
+    if (!this.head) {
+      this.insertFirst(data);
+    } else {
+      var last = this.getLast();
+      last.next = new Node(data);
+    }
+  }
+
+  getAt(n) {
+    if (!this.head || n > this.size() - 1) {
+      return null;
+    }
+
+    var count = 0;
+    var node = this.head;
+
+    while (count < n) {
+      count++;
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  removeAt(n) {
+    if (!this.head || n > this.size() - 1) {
+      return null;
+    } else if (n === 0) {
+      this.head = this.head.next;
+      return;
+    }    
+
+    var previous = this.getAt(n - 1);
+    var node = this.getAt(n);
+    previous.next = node.next;
+  }
+
+  insertAt(data, index) {
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    } else if (index >= this.size()) {
+      this.insertLast(data);
+      return;
+    }
+    
+    var previous = this.getAt(index - 1);
+    var next = this.getAt(index);
+
+    previous.next = new Node(data, next);
+  }
+
+  forEach(callback) {
+    for (var i = 0; i < this.size(); i++) {
+      var node = this.getAt(i);
+      node = callback(node);
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
   }
 }
 
--- Common Coding Interview Problems/Basic_Algorithms/exercises/linkedlist/test.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/linkedlist/test.js
@@ -98,7 +98,7 @@ describe('RemoveFirst', () => {
   });
 });
 
-describe.skip('RemoveLast', () => {
+describe('RemoveLast', () => {
   test('RemoveLast removes the last node when list is empty', () => {
     const l = new List();
     expect(() => {
@@ -136,7 +136,7 @@ describe.skip('RemoveLast', () => {
   });
 });
 
-describe.skip('InsertLast', () => {
+describe('InsertLast', () => {
   test('adds to the end of the list', () => {
     const l = new List();
     l.insertFirst('a');
@@ -148,7 +148,7 @@ describe.skip('InsertLast', () => {
   });
 });
 
-describe.skip('GetAt', () => {
+describe('GetAt', () => {
   test('returns the node at given index', () => {
     const l = new List();
     expect(l.getAt(10)).toEqual(null);
@@ -165,7 +165,7 @@ describe.skip('GetAt', () => {
   });
 });
 
-describe.skip('RemoveAt', () => {
+describe('RemoveAt', () => {
   test('removeAt doesnt crash on an empty list', () => {
     const l = new List();
     expect(() => {
@@ -218,7 +218,7 @@ describe.skip('RemoveAt', () => {
   });
 });
 
-describe.skip('InsertAt', () => {
+describe('InsertAt', () => {
   test('inserts a new node with data at the 0 index when the list is empty', () => {
     const l = new List();
     l.insertAt('hi', 0);
@@ -273,7 +273,7 @@ describe.skip('InsertAt', () => {
   });
 });
 
-describe.skip('ForEach', () => {
+describe('ForEach', () => {
   test('applies a transform to each node', () => {
     const l = new List();
 
@@ -293,7 +293,7 @@ describe.skip('ForEach', () => {
   });
 });
 
-describe.skip('for...of loops', () => {
+describe('for...of loops', () => {
   test('works with the linked list', () => {
     const l = new List();
 
