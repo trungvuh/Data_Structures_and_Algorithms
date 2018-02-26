140e8e333d1aa221a55ea2ff0c692ea67073bb3d
TV <trungvuh@gmail.com>
2018-02-26 13:40:42 -0800

    Tree and BFS-DFS in JS

--- Common Coding Interview Problems/Basic_Algorithms/exercises/tree/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/tree/index.js
@@ -11,11 +11,55 @@
 // function that gets called with each element in the tree
 
 class Node {
+  constructor(data, children = []) {
+    this.data = data;
+    this.children = children;
+  }
 
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    // for (var i = 0, n = this.children.length; i < n; i++) {
+    //   if (this.children[i].data === data) {
+    //     this.children.pop(this.children[i]);
+    //   }
+    // }
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
 }
 
 class Tree {
-  
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(func) {
+    let nodes = [this.root];
+
+    while (nodes.length) {
+      let node = nodes.shift();
+      // for (let child of node.children) {
+      //   nodes.push(child);
+      // }
+      // or we can use the rest operator like so:
+      nodes.push(...node.children);
+      func(node);
+    }
+  }
+
+  traverseDF(func) {
+    let nodes = [this.root];
+
+    while (nodes.length) {
+      let node = nodes.shift();
+      nodes.unshift(...node.children);
+      func(node);
+    }
+  }
 }
 
 module.exports = { Tree, Node };
