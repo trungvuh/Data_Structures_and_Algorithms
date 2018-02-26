911e31f2e007cebc3a321c1ddf922f66d19b916e
TV <trungvuh@gmail.com>
2018-02-26 15:58:53 -0800

    Implement BST and traverse BST in JS

--- Common Coding Interview Problems/Basic_Algorithms/exercises/bst/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/bst/index.js
@@ -11,6 +11,40 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (!this.left && data < this.data) {
+      this.left = new Node(data);
+    } else if (data < this.data) {
+      this.left.insert(data);
+    } else if (!this.right && data >= this.data) {
+      this.right = new Node(data);
+    } else if (data >= this.data) {
+      this.right.insert(data);
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this;
+    }
+
+    if (data < this.data && !this.left) {
+      return null;
+    } else if (data < this.data) {
+      return this.left.contains(data);
+    } else if (data > this.data && !this.right) {
+      return null;
+    } else if (data > this.data) {
+      return this.right.contains(data);
+    }
+  }
+}
 
 module.exports = Node;
--- Common Coding Interview Problems/Basic_Algorithms/exercises/levelwidth/index.js
+++ Common Coding Interview Problems/Basic_Algorithms/exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root, "s"];
+  let counters = [0];
+
+  while (arr.length > 1) {
+    let node = arr.shift();
+
+    if (node === "s") {
+      // if element is a signified stopper
+      arr.push("s");
+      counters.push(0);
+    } else {
+      // else, element is a regular node
+      counters[counters.length - 1] += 1;
+      arr.push(...node.children);
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
